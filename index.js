2ffa07c35eb1ed3dc2dc16a6bfe07eb57d52d2e5
Brian Sarango <brian.sarango@baruchmail.cuny.edu>
2023-05-30 00:19:27 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(firstIndex=0; firstIndex<array.length-1; firstIndex++){
+    for(secondIndex=firstIndex+1; secondIndex<array.length; secondIndex++){
+      if(array[firstIndex]+array[secondIndex]===target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  input an array of numbers and a target that you want to see if any values in the array added up will sum up to it
+  you iterate through the array till the second to last item
+  within each iteration, you iterate starting from the next point through every element after it
+  you sum the first iteration and the second iteration to see if there is any sum that matches the target
+  if not, you return false
 */
 
 /*
   Add written explanation of your solution here
+  The code will iterate from each value of the array till the second to last value and once again iterate from the second till last item
+  It will add these two values through each iteration to see if they match the target.
 */
 
 // You can run `node index.js` to view these console logs
